29b0f150fd021d6133a63531cf03633fb80bf605
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2025-03-01 22:52:23 -0500

    testing structure
    added comprehensive testing structure

--- app.py
+++ app.py
@@ -1,22 +1,69 @@
-from flask import Flask, request, jsonify
+# app.py
+# handles app routing, (TODO) redirecting to utils and chat functions
+
+# Import statements
+import os
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 from config import get_logger
 
 _LOGGER = get_logger(__name__)
-_LOGGER.info("Application started")
+_LOGGER.info("Application starting...")
+
+# Check for dev state.
+try:
+    _ENV      = os.environ.get("flaskEnv")
+    _DEV_HOST = os.environ.get("flaskHost")
+    _DEV_PORT = os.environ.get("flaskPort")
+    _DEV_PAGE = os.environ.get("flaskPage")
+    _DEV_ADDR = "http://" + _DEV_HOST + ":" + _DEV_PORT + "/query"
+except:
+    _LOGGER.info("flaskEnv not found...setting to production environment.")
+    _ENV = ""
 
 app = Flask(__name__)
+CORS(app) # Potentially unnecessary but ok to include.
 
+# Main app route; how to query the chatbot and get responses back.
 @app.route('/query', methods=['POST'])
-def main():    
-    return jsonify({"text":"boilerplate init response"})
+def main():  
+    # Enforce only JSON requests
+    if not request.is_json:
+        _LOGGER.warning("[SECURITY] Non-JSON request blocked.")
+        return jsonify({"error": "Invalid content type"}), 400
+  
+    # TODO: User data collection
+    # TODO: Welcome if user is new
+    # TODO: Query LLMProxy
     
+    return jsonify({"text":"_\[markdown enabled\]_\n###Boilerplate init response - check back later."})
+
+# Dev route; displays a basic prompt/response page that uses /query
+@app.route('/dev')
+def dev():
+    # Restricts access if environment variables improperly setup
+    if _ENV != "dev":
+        _LOGGER.warning("Attempted access to /dev but not in dev environment.")
+        return default()
+
+    _LOGGER.info(f"Serving dev page to {_DEV_ADDR}")
+    return render_template(template_name_or_list=_DEV_PAGE, address=_DEV_ADDR)  
+
+# Default page
 @app.route('/')
-def hello_world():
+def default():   
    return jsonify({"message":'There is nothing on this page. Please return to where you came from!'})
 
+# Error page
 @app.errorhandler(404)
 def page_not_found(e):
     return "Error 404: Page Not Found", 404
 
+# App startup
 if __name__ == "__main__":
-    app.run(debug=True)
+    # If dev state, run verbosely and host locally
+    if _ENV == "dev":
+        app.run(debug=True, use_reloader=True, host=_DEV_HOST, port=_DEV_PORT)
+    else:
+        app.run(debug=True)
+        
